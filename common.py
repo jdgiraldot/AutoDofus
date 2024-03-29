06291672e33054bd8b1ddedf989ce0f8aff25873
Jhonatan Giraldo <jdgiraldot@outlook.com>
2024-03-29 13:07:01 -0500

    Ajuste tiempos

--- common.py
+++ common.py
@@ -7,19 +7,19 @@ def initial_status(status):
         if pyautogui.pixelMatchesColor(1355, 1007, (173, 173, 173)): # Activa modo criaturas
             pyautogui.hotkey('shift', '%')
         pyautogui.press('f1')
-        time.sleep(0.5)
+        time.sleep(1)
         if pyautogui.pixelMatchesColor(260, 420, (191, 231, 0)): # Seleccionar reto
-            pyautogui.press('f1')      
+            pyautogui.press('f1')
         print("Initial status: ", status)
     else:
         status = "Gathering"
         print("Initial status: ", status)
     
-    time.sleep(3)
+    time.sleep(8)
     return status
 
 def validate_combat_status(status):
-    if pyautogui.pixelMatchesColor(910, 720, (244, 226, 47)): # Letreto de Victoria (910, 720, (244, 226, 47)) o (930, 720, (243, 224, 44))
+    if pyautogui.pixelMatchesColor(910, 720, (244, 226, 47)): # Letreto de Victoria
         status = "Gathering"
         pyautogui.press('esc')
         print("New status: ", status)
@@ -42,11 +42,11 @@ def validate_gathering_status(status):
         if pyautogui.pixelMatchesColor(1355, 1007, (173, 173, 173)): # Activa modo criaturas
             pyautogui.hotkey('shift', '%')
         pyautogui.press('f1')
-        time.sleep(0.5)
+        time.sleep(1)
         if pyautogui.pixelMatchesColor(260, 420, (191, 231, 0)): # Seleccionar reto
             pyautogui.press('f1')
         print("New status: ", status)
-        time.sleep(4)
+        time.sleep(7)
     
-    time.sleep(0.5)
+    time.sleep(1)
     return status
